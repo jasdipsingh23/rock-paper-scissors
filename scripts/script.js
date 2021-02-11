4fb99acba0da0f4b029cfd66a1cc121315479b4c
Jasdip <jasdipsingh23@gmail.com>
2021-02-11 09:38:01 -0800

    Added comments and commented-out unused code (console logic)

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,15 @@ function computerPlay() {
     return options[randomIndex(options.length)];
 }
 
-// Helper function returns random integer less than maxSize parameter
-// 0 <= returnInteger < maxSize
+// Helper function
+// Returns random integer less than maxSize parameter
+//  0 <= returnInteger < maxSize
 function randomIndex(maxSize) {
     return Math.floor(Math.random() * Math.floor(maxSize));
 }
 
 // Compares player selection to computer selection
-// Returns the result of options given as a String
+// Returns the result as a String
 function playRound(playerSelection, computerSelection) {
     let result = "";
 
@@ -43,11 +44,87 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
-// Intro to game 
+// Lets user play game by clicking buttons on webpage
+const buttons = Array.from(document.querySelectorAll("button"));
+buttons.forEach(button => button.addEventListener("click", () => {
+    let computerSelection = computerPlay();
+    let roundResult = playRound(button.id, computerSelection);
+    
+    displayRoundResult(roundResult);
+    displayScore(roundResult);
+}));
+
+// Displays the result of the round
+function displayRoundResult(roundResult) {
+    removePreviousChildren("result");
+
+    const resultDiv = document.getElementById("result");
+    const result = document.createElement("h2");
+    result.textContent = roundResult;
+    resultDiv.appendChild(result);
+}
+
+// Removes any children associated to the id parameter
+function removePreviousChildren(id) {
+    const previousRoundResult = document.getElementById(id);
+    while(previousRoundResult.hasChildNodes()) {
+        previousRoundResult.removeChild(previousRoundResult.firstChild);
+    }
+}
+
+// Global variables to hold overall game score
+let PLAYER_SCORE = 0;
+let COMPUTER_SCORE = 0;
+
+// Displays current score
+function displayScore(roundResult) {
+    removePreviousChildren("score");
+
+    roundResult = roundResult.split(" ");
+    if(roundResult[1] === "Won!") {
+        PLAYER_SCORE += 1;
+    } else if(roundResult[1] === "Lost!") {
+        COMPUTER_SCORE += 1;
+    } 
+
+    const scoreDiv = document.getElementById("score");
+    const playerScoreElement = document.createElement("h3");
+    const computerScoreElement = document.createElement("h3");
+    
+    playerScoreElement.textContent = "Your Score: " + PLAYER_SCORE;
+    computerScoreElement.textContent = "Computer's Score: " + COMPUTER_SCORE;
+
+    scoreDiv.appendChild(playerScoreElement);
+    scoreDiv.appendChild(computerScoreElement);
+
+    gameEnd(scoreDiv);
+}
+
+// Displays winner after 5 rounds are played
+// Resets scores to 0
+function gameEnd(scoreDiv) {
+    if(PLAYER_SCORE === 5 || COMPUTER_SCORE === 5) {
+        PLAYER_SCORE = 0;
+        COMPUTER_SCORE = 0;
+
+        const gameResult = document.createElement("h1");
+        gameResult.textContent = (PLAYER_SCORE === 5 ? "YOU WIN!" : "YOU LOSE!");
+        gameResult.textContent += " Play again?";
+
+        scoreDiv.appendChild(gameResult);
+        
+    }
+}
+
+/* NOT USED: LOGIC TO PLAY ROCK PAPER SCISSORS GAME IN CONSOLE ONLY
+
+// Intro to game
 function gameIntro() {
     console.log("Let's play Rock, Paper, or Scissors! Best of 5 wins!");
 }
+*/
 
+/*
 // Logs result of player and computer scores
 function gameEnd(playerScore, computerScore) {
     console.log("Your score: " + playerScore);
@@ -58,7 +135,9 @@ function gameEnd(playerScore, computerScore) {
         console.log("You Lose!");
     }
 }
+*/
 
+/*
 // Prompts user for selection
 // Continues to prompt user if invalid selection given
 function playerPlay() {
@@ -71,6 +150,7 @@ function playerPlay() {
 
     return playerSelection;
 }
+*/
 
 /*
 // Plays 1 complete rounds of rock-paper-scissors
@@ -100,67 +180,3 @@ function game() {
     gameEnd(playerScore, computerScore);
 }
 */
-
-// Lets user play game by clicking buttons on webpage
-const buttons = Array.from(document.querySelectorAll("button"));
-buttons.forEach(button => button.addEventListener("click", () => {
-    let computerSelection = computerPlay();
-    let roundResult = playRound(button.id, computerSelection);
-    
-    displayRoundResult(roundResult);
-    displayScore(roundResult);
-}));
-
-// Displays the result of the round.
-function displayRoundResult(roundResult) {
-    removePreviousChildren("result");
-
-    const resultDiv = document.getElementById("result");
-    const result = document.createElement("h2");
-    result.textContent = roundResult;
-    resultDiv.appendChild(result);
-}
-
-// Removes any children associated to the id parameter
-function removePreviousChildren(id) {
-    const previousRoundResult = document.getElementById(id);
-    while(previousRoundResult.hasChildNodes()) {
-        previousRoundResult.removeChild(previousRoundResult.firstChild);
-    }
-}
-
-let PLAYER_SCORE = 0;
-let COMPUTER_SCORE = 0;
-
-function displayScore(roundResult) {
-    removePreviousChildren("score");
-
-    roundResult = roundResult.split(" ");
-    if(roundResult[1] === "Won!") {
-        PLAYER_SCORE += 1;
-    } else if(roundResult[1] === "Lost!") {
-        COMPUTER_SCORE += 1;
-    } 
-
-    const scoreDiv = document.getElementById("score");
-    const playerScoreElement = document.createElement("h3");
-    const computerScoreElement = document.createElement("h3");
-    
-    playerScoreElement.textContent = "Your Score: " + PLAYER_SCORE;
-    computerScoreElement.textContent = "Computer's Score: " + COMPUTER_SCORE;
-
-    scoreDiv.appendChild(playerScoreElement);
-    scoreDiv.appendChild(computerScoreElement);
-
-    if(PLAYER_SCORE === 5 || COMPUTER_SCORE === 5) {
-        PLAYER_SCORE = 0;
-        COMPUTER_SCORE = 0;
-
-        const gameResult = document.createElement("h1");
-        gameResult.textContent = (PLAYER_SCORE === 5 ? "YOU WIN!" : "YOU LOSE!");
-        gameResult.textContent += " Play again?";
-
-        scoreDiv.appendChild(gameResult);
-        
-    }
-}
